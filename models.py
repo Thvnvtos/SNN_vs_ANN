8ff5c8e8cd703b1e1f5ff0975f8b3eafe0fe78c8
Ilyass HAMMOUAMRI <Hammouamri.ilyass@gmail.com>
2021-03-25 20:26:21 +0100

    Added ANN model, and train_full.py for ANN

---
+++ models.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+class ANN(nn.Module):
+    # in_c : input channels, fm : feature maps, ks : kernel size
+    def __init__(self, in_c = 1,fm = [8,16], ks=[5,5]):
+        super().__init__()
+        
+        self.convLayer1 = nn.Sequential(
+            nn.Conv2d(in_c, fm[0], kernel_size=ks[0]),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+            )
+        self.dim1 = (28 - ks[0] + 1) // 2
+        self.convLayer2 = nn.Sequential(
+            nn.Conv2d(fm[0],fm[1], kernel_size=ks[1]),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+            )
+        self.dim2 = (self.dim1 - ks[1] + 1) // 2
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(fm[1]*self.dim2**2, 10),
+            nn.Softmax()
+            )
+
+    def forward(self, x):
+        x = self.convLayer1(x)
+        x = self.convLayer2(x)
+        return self.fc(x)
+
+
+class SNN(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
---
+++ train_full.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import models
+
+
+epochs = 100
+batch_size = 64
+lr = 1e-2
+log_interval = 14000 // batch_size # in order to have 5 logs in each epoch depending on the batch_size
+
+data_root = "data"
+seed = 11
+
+mnist_mean = 0.1307
+mnist_std = 0.3081
+
+
+def train_full(net, train_loader, optimizer, device, epoch):
+	
+	net.train()
+	correct_pred = 0
+
+	for batch_idx, (x, label) in enumerate(train_loader):
+		x, label = x.to(device), label.to(device)
+		optimizer.zero_grad()
+		y = net(x)
+		loss = F.cross_entropy(y, label)
+		pred = y.argmax(dim=1)
+		correct_pred += (pred == label).sum().item()
+		loss.backward()
+		optimizer.step()
+
+		if batch_idx % log_interval == 0:
+			print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(epoch, batch_idx * len(x), len(train_loader.dataset),
+				100. * batch_idx / len(train_loader), loss.item()))
+
+	print("===> Train Epoch Accuracy : {:.0f}%".format(100. * correct_pred / len(train_loader.dataset)))
+
+def test_full(net, test_loader, device):
+	net.eval()
+	test_loss = 0
+	correct_pred = 0
+	with torch.no_grad():
+		for x, label in test_loader:
+			x, label = x.to(device), label.to(device)
+			y = net(x)
+			test_loss += F.cross_entropy(y, label).item()
+			pred = y.argmax(dim=1)
+			correct_pred += (pred == label).sum().item()
+
+	print("===> Test Accuracy : {:.0f}%, Test Average loss: {:.4f}".format(100. * correct_pred / len(test_loader.dataset), 
+		test_loss / len(test_loader.dataset)))
+
+if __name__ == '__main__':
+
+	torch.manual_seed(seed)
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	print("Training on {} !".format(torch.cuda.get_device_name()))
+
+	transform=transforms.Compose([
+		transforms.ToTensor(),
+		transforms.Normalize((mnist_mean,), (mnist_std,))
+		])
+
+	dataset_train = datasets.MNIST(data_root, train=True, transform=transform)
+	dataset_test = datasets.MNIST(data_root, train=False,transform=transform)
+
+	train_loader = torch.utils.data.DataLoader(dataset_train, batch_size, shuffle=True)
+	test_loader = torch.utils.data.DataLoader(dataset_test, batch_size)
+
+	net = models.ANN().to(device)
+
+	optimizer = optim.SGD(net.parameters(), lr = lr)
+
+	for epoch in range(epochs):
+		train_full(net, train_loader, optimizer, device, epoch+1)
+		test_full(net, test_loader, device)
+		print("------------------------------------------------------")
